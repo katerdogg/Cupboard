434dbace185292e3d93a7793ae7f21282d850525
katerdogg <katiemariedover@gmail.com>
2016-03-24 11:53:11 -0700

    comming changes to cupboard capstone project

--- cupboard/cupboard_app/models.py
+++ cupboard/cupboard_app/models.py
@@ -1,9 +1,20 @@
+"""Models for Cupboard Project"""
+
 from django.db import models
 
-class Cup_User():
+class Cup_User(models.Model):
+	"""Holds all data for the user"""
+	 avatar = 
+	 name = models.CharField(max_length=200)
+
+class Fav_Recipe(models.Model):
+	"""Holds all Fav Recipe data"""
+	recipe =
+	description = 
+	user_note =
 
 
-class Fav_Recipe():
-	"""docstring for ClassName"""
-
-class Fav_Search():
+class Fav_Search(models.Model):
+	"""Holds all Fav Search data"""
+	search_terms = 
+	user_note = 
