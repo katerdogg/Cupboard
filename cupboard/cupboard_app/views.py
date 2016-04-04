688083eea9080880c1835bb637ede8f15feb2fec
katerdogg <katiemariedover@gmail.com>
2016-04-04 11:33:37 -0700

    commented views in django

--- cupboard/cupboard_app/views.py
+++ cupboard/cupboard_app/views.py
@@ -13,10 +13,17 @@ def search(request):
 	return render(request, "cupboard_app/search.html")
 
 def results(request):
+	"""Results page for the search from the user"""
 	context_dict = {}
+	# If requested (if the submit button has been clicked)
 	if request.method == "POST":
+		# var to hold the ingredients entered into the form on the search page; split each ingredient by
+			# a comma and a space
 		search_list = request.POST["ingredients"].split(", ")
+		# var to hold the api id and key
 		client = Client(api_id="243f40b9", api_key="0b99d35c38f2043e163a04a97e9c5476", timeout=5.0, retries=0)
+		
+		# parameters for each search.
 		params = {
 	    'q': search_list,
 	    'start': 0,
@@ -33,7 +40,9 @@ def results(request):
 	    # 'nutrition.FAT.min': 0,
 	    # 'nutrition.FAT.max': 15,
 		}
+		# var results to hold the client search parameters; **params to hold as many or as few as it needs
 		results = client.search(**params)
+		# var context_dict to hold a dictionary of each result match with the key "results"
 		context_dict = {"results": results.matches}
-	
+
 	return render(request, "cupboard_app/results.html", context_dict)
