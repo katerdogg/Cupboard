5b74d7cd413df3e5c88b29e2d911e4e333fc3ece
katerdogg <katiemariedover@gmail.com>
2016-04-21 17:32:20 -0700

    integrated new parallax scrolling. Must fix the obvious conflicting css

--- cupboard/cupboard_app/static/cupboard_app/results.js
+++ cupboard/cupboard_app/static/cupboard_app/results.js
@@ -1,11 +1,39 @@
 /**
- * JS creating a grid to display each results image
+ * JS for Cupboard
  **/
-
 
 /**
- * Loop through each result to display the recipe photos
+ * Parallax Scrolling JavaScript
  **/
-function results() {
-	
-}
+
+
+$(document).ready(function(){
+    // Cache the Window object
+    $window = $(window);
+                
+   $('section[data-type="background"]').each(function(){
+     var $bgObj = $(this); // assigning the object
+                    
+      $(window).scroll(function() {
+                    
+        // Scroll the background at var speed
+        // the yPos is a negative value because we're scrolling it UP!                              
+        var yPos = -($window.scrollTop() / $bgObj.data('speed'));
+        
+        // Put together our final background position
+        var coords = '50% '+ yPos + 'px';
+
+        // Move the background
+        $bgObj.css({ backgroundPosition: coords });
+        
+}); // window scroll Ends
+
+ });
+
+});
+/* 
+ * Create HTML5 elements for IE's sake
+ */
+
+document.createElement("article");
+document.createElement("section");
